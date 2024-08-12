57b43ba5a0002645443eb499a437286b3e78bdbb
Royal <royalhuseyn902@gmail.com>
2024-08-13 01:40:14 +0400

    Fix validateAdminPW

--- middleware/validateAdminPW.js
+++ middleware/validateAdminPW.js
@@ -1,10 +1,11 @@
 const { body } = require('express-validator');
+require('dotenv').config();
 
 module.exports = [
   body('pw')
     .trim()
     .notEmpty()
     .withMessage('Password cannot be empty')
-    .equals('Royal2002@@23')
+    .equals(process.env.ADMIN_PW)
     .withMessage('Incorrect password'),
 ];
